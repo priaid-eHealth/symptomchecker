900ae67cf829415697638450c3e20f4ce96edb8b
priaid <github@priaid.ch>
2017-07-12 17:58:32 +0200

    New language: sk-sk
    New language Slovakian (sk-sk) included

--- Python/config.py
+++ Python/config.py
@@ -4,5 +4,5 @@ username = "YOUR_USERNAME" # Here enter your API user name
 password = "YOUR_PASSWORD" # Here enter your API password
 priaid_authservice_url = "https://sandbox-authservice.priaid.ch/login" # Be aware that sandbox link is for testing pourpose (not real data) once you get live access you shold use https://authservice.priaid.ch/login
 priaid_healthservice_url = "https://sandbox-healthservice.priaid.ch" # Be aware that sandbox link is for testing pourpose (not real data) once you get live access you shold use https://healthservice.priaid.ch
-language = "en-gb" # en-gb, de-ch, fr-fr, es-es, tr-tr...
+language = "en-gb" # en-gb, de-ch, fr-fr, es-es, tr-tr, sk-sk...
 pritnRawOutput = False # This flag can be set to see printed json data structure of webservice responses
