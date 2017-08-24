1a2d33f83231d8ea66948c8e9c36179a86d3a92a
priaid <github@priaid.ch>
2017-08-24 16:34:28 +0200

    New language: ru-ru
    New language Russian (ru-ru) included

--- Angular/scripts/controllers/diagnosiscontroller.js
+++ Angular/scripts/controllers/diagnosiscontroller.js
@@ -60,7 +60,7 @@
 		vm.redFlagTextError = '';
 		vm.symptomId = 238;
 		
-		vm.languages=[{value:"en-gb",name:"en-gb"},{value:"de-ch",name:"de-ch"},{value:"fr-fr",name:"fr-fr"},{value:"es-es",name:"es-es"},{value:"tr-tr",name:"tr-tr"},{value:"sk-sk",name:"sk-sk"}]
+		vm.languages=[{value:"en-gb",name:"en-gb"},{value:"de-ch",name:"de-ch"},{value:"fr-fr",name:"fr-fr"},{value:"es-es",name:"es-es"},{value:"ru-ru",name:"ru-ru"},{value:"tr-tr",name:"tr-tr"},{value:"sk-sk",name:"sk-sk"}]
 		//Setting first option as selected in configuration select
 		vm.lang = vm.languages[0].value;
 		
