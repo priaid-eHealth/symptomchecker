4bef87f7969edc0bf5f9ad28a739edba378d9e41
priaid <github@priaid.ch>
2017-09-06 18:04:13 +0200

    Correction naming
    Correction of wrong naming

---
+++ Java/Priaid/src/Priaid/Diagnosis/Model/DiagnosedSpecialisation.java
@@ -0,0 +1,10 @@
+package Priaid.Diagnosis.Model;
+
+public class DiagnosedSpecialisation extends HealthItem{
+	
+	/// <summary>
+    /// ID of specialisation
+    /// </summary>
+	public Integer SpecialistID;
+	
+}
